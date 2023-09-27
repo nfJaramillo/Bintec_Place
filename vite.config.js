01c732c85ea1bbeab2c72d1c591fab16c0cdae96
Nicolás F. Jaramillo <njaramil@bancolombia.com.co>
2023-09-27 09:56:48 -0500

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Bintec_Place/',
   plugins: [react(), eslint()],
   define: {
     global: {},
